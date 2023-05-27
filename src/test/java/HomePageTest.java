23f826fac52560f97165176bf9fe8c799c053989
Andrew <someo44ne@gmail.com>
2023-05-27 17:19:07 +0300

    Final version of HW

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -20,7 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static utils.PropertiesFactory.getProperty;
 
 
-public class HomePageTest extends MainTest {
+public class HomePageTest {
 
     private WebDriver driver;
     private CommonPageElements commonPageElements;
--- src/test/java/MainTest.java
+++
@@ -1,8 +0,0 @@
-import org.testng.annotations.*;
-
-public class MainTest {
-
-
-
-
-}
